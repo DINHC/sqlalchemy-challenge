a52d2d7513d8d42c420bdba94714d43680428a5f
Dinh Chau <dinhchau001@gmail.com>
2021-07-26 18:38:52 -0700

    Fixed issue with being unable to jump route now need to fix issue with start and start/end returning null

--- app.py
+++ app.py
@@ -20,9 +20,17 @@ session = Session(engine)
 
 app = Flask(__name__)
 
+# def calc_temps(start_date, end_date):
+#     return session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+#         filter(Measurement.date >= start_date).filter(Measurement.date <= end_date).all()
+#Placed into start
+def calc_temps(start_date, end_date):
+    return session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start_date).all()
+
 @app.route("/")
-def intro():
-    "All APIs"
+def main():
+    """All APIs"""
     return(
         f"Available Routes:<br/>"
         f"/api/v1.0/precipitation<br/>"
@@ -31,76 +39,84 @@ def intro():
         f"/api/v1.0/<start><br/>"
         f"/api/v1.0/<start>/<end>")  
     
-@app.route("/api/v1.0/precipitation")    
+@app.route("/api/v1.0/precipitation")
 def precipitation():
-    recent_year = dt.date(2017,8,23) - dt.timedelta(days = 365)
-    last_day = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
-    precipitation = session.query(Measurement.date, Measurement.prcp).\
-    filter(Measurement.date > recent_year).order_by(Measurement.date).all()
-    # prcp_dict = {}
-    # for result in prcp_data:
-    #   prcp_dict[precipitation[0]] = precipitation[1]
-    prcp_data = []
-    for i in precipitation:
-        data = {}
-        data['date'] = precipitation[0]
-        data['prcp'] = precipitation[1]
-        prcp_data.append(data)
-    return jsonify(prcp_data)
+    final_date_query = session.query(func.max(func.strftime("%Y-%m-%d", Measurement.date))).all()
+    max_date_string = final_date_query[0][0]
+    max_date = dt.datetime.strptime(max_date_string, "%Y-%m-%d")
+
+    begin_date = max_date - dt.timedelta(365)
+
+    precipitation_data = session.query(func.strftime("%Y-%m-%d", Measurement.date), Measurement.prcp).\
+        filter(func.strftime("%Y-%m-%d", Measurement.date) >= begin_date).all()
+    session.close()
+
+    results_dict = {}
+    for result in precipitation_data:
+        results_dict[result[0]] = result[1]
+    return jsonify(results_dict)
 
 @app.route("/api/v1.0/stations")
-def station():
-    station_data = session.query(Station.station).all()
-    all_station = list(np.ravel(station_data))
-    return jsonify(all_station)
+def stations():
+    stations = session.query(Station).all()
+    session.close()
+    stations_list = []
+    for station in stations:
+        station_dict = {}
+        station_dict["id"] = station.id
+        station_dict["station"] = station.station
+        station_dict["name"] = station.name
+        station_dict["latitude"] = station.latitude
+        station_dict["longitude"] = station.longitude
+        station_dict["elevation"] = station.elevation
+        stations_list.append(station_dict)
+
+    return jsonify(stations_list)
 
 @app.route("/api/v1.0/tobs")
 def tobs():
-    tobs_results = session.query(Measurement.station, Measurement.tobs).\
-        filter(Measurement.dt.between('2016-08-23', '2017-08-23')).all()
+    final_date_query = session.query(func.max(func.strftime("%Y-%m-%d", Measurement.date))).all()
+    max_date_string = final_date_query[0][0]
+    max_date = dt.datetime.strptime(max_date_string, "%Y-%m-%d")
+    begin_date = max_date - dt.timedelta(365)
+    results = session.query(Measurement).\
+        filter(func.strftime("%Y-%m-%d", Measurement.date) >= begin_date).all()
+    session.close()
+    
     tobs_list = []
-    for i in tobs:
-        dict = {}
-        dict["station"] = tobs[0]
-        dict["tobs"] = float(tobs[1])
-        list.append(dict)                             
+    for result in results:
+        tobs_dict = {}
+        tobs_dict["date"] = result.date
+        tobs_dict["station"] = result.station
+        tobs_dict["tobs"] = result.tobs
+        tobs_list.append(tobs_dict)
     return jsonify(tobs_list)
 
-# @app.route("/api/v1.0/<start>")
-# def temp_start(start):
-#     start = dt.strptime('2016-08-23', '%Y-%m-%d').date()
-#     start_results = session.query(func.avg(Measurement.tobs),func.max(Measurement.tobs),func.min(Measurement.tobs).filter(Measurement.date >= start)
-# start_list = []
-# date_dict = {'start_date': start, 'end_date': max_date}
-#         start_list.append(date_dict)
-#         start_list.append({'Observation': 'TEMPMIN', 'Temperature': temps[0][0]})
-#         start_list.append({'Observation': 'TEMPAVG', 'Temperature': temps[0][1]})
-#         start_list.append({'Observation': 'TEMPMAX', 'Temperature': temps[0][2]})
-# return jsonify(start_list) 
-#     start_list = []   
-#     for i in start_results:
-#         start_dict = {}
-#         start_dict["TMIN"] = float(tobs[1])                     
-#         start_dict["TMAX"] = float(tobs[0])
-#         start_dict["TAVG"] = float(tobs[2])
-#         list.append(start_dict)
-#     return jsonify(start_list)
+@app.route("/api/v1.0/<start>")
+def start(start):
+    final_date = session.query(func.max(func.strftime("%Y-%m-%d", Measurement.date))).all()
+    session.close()
+    last_date = final_date[0][0]
+    temps = calc_temps(start, last_date)
+    return_list = []
+    date_dict = {'start_date': start, 'end_date': last_date}
+    return_list.append(date_dict)
+    return_list.append({'Observation': 'TMIN', 'Temperature': temps[0][0]})
+    return_list.append({'Observation': 'TAVG', 'Temperature': temps[0][1]})
+    return_list.append({'Observation': 'TMAX', 'Temperature': temps[0][2]})
+    return jsonify(return_list)
 
-# @app.route("/api/v1.0/<start>/<end>")
-# def temps_end(start,end):
-#     start = dt.strptime('2016-08-23', '%Y-%m-%d').date()                      
-#     end = dt.strptime('2017-08-23', '%Y-%m-%d').date()
-#     end_results = session.query(func.avg(Measurement.tobs),func.max(Measurement.tobs),func.min(Measurement.tobs).\
-#                filter(Measurement.date >= start)                     
-#     start_end_tobs_list = []
-#     for i in start_end_tobs_list:
-#        dict = {}
-#        dict["TMIN"] = float(tobs[1])                     
-#        dict["TMAX"] = float(tobs[0])
-#        dict["TAVG"] = float(tobs[2])
-#        start_end_tobs_list.append(dict)
-#     return jsonify(start_end_tobs_list)   
-                
+@app.route("/api/v1.0/<start>/<end>")
+def start_end(start, end):
+    temps = calc_temps(start, end)
+    return_list = []
+    date_dict = {'start_date': start, 'end_date': end}
+    return_list.append(date_dict)
+    return_list.append({'Observation': 'TMIN', 'Temperature': temps[0][0]})
+    return_list.append({'Observation': 'TAVG', 'Temperature': temps[0][1]})
+    return_list.append({'Observation': 'TMAX', 'Temperature': temps[0][2]})
+    session.close()
+    return jsonify(return_list)
+
 if __name__ == "__main__":
-    app.run(debug = True)   
-
+    app.run(debug=True)
