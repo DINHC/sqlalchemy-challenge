3213114a346c461513dfc8eb130143d63f7729b9
Dinh Chau <dinhchau001@gmail.com>
2021-07-28 19:52:25 -0700

    I have tried to get the start/end route code to work but to no avail. Start route works fine

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-#Code works for opening website, Can open one API and when trying to open another the site crashes due to an error. Restart the code allows me to open one API
-# Either precipitation or station 
+#Code works for opening website, and allows for mst route to open except Start/end
 from flask import Flask, jsonify
 import numpy as np
 import pandas as pd
@@ -20,13 +19,13 @@ session = Session(engine)
 
 app = Flask(__name__)
 
-def calc_temps(start_date, end_date):
-    return session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
-        filter(Measurement.date >= start_date).filter(Measurement.date <= end_date).all()
-#Placed into start
 # def calc_temps(start_date, end_date):
 #     return session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
-#         filter(Measurement.date >= start_date).all()
+#         filter(Measurement.date >= start_date).filter(Measurement.date <= end_date).all()
+#Placed into start
+def calc_temps(start_date, end_date):
+    return session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start_date).all()
 
 @app.route("/")
 def main():
@@ -95,7 +94,7 @@ def tobs():
 @app.route("/api/v1.0/<start>")
 @app.route("/api/v1.0/<start>/<end>")
 def data(start=None, end=None):
-    tobs_data = [func.min(Measurement.tobs),func.avg(Measurement.tobs),func.max(Measurement.tobs)]
+
     if not end: 
         final_date_query = session.query(func.max(func.strftime("%Y-%m-%d", Measurement.date))).all()
         session.close()
@@ -110,28 +109,34 @@ def data(start=None, end=None):
         return_list.append({'Observation': 'TAVG', 'Temperature': temps[0][1]})
         return_list.append({'Observation': 'TMAX', 'Temperature': temps[0][2]})
         return jsonify(return_list)
-    
-    start = dt.datetime.strptime(start, "%Y-%m-%d")
-    end = dt.datetime.strptime(end, "%Y-%m-%d")
 
-    data_results = session.query(tobs_data).filter(Measurement.date >= start).filter(Measurement.date <= end).all() 
+def final_calc(start_date, end_date): 
+    session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start_date).filter(Measurement.date <= end_date).all()
     session.close()
-    temps = tobs_data(start, end)
-    return_list = []
-    date_dict = {"end_date": end}
-    return_list.append(date_dict)
-    return_list.append({'Observation': 'TMIN', 'Temperature': temps[0][0]})
-    return_list.append({'Observation': 'TAVG', 'Temperature': temps[0][1]})
-    return_list.append({'Observation': 'TMAX', 'Temperature': temps[0][2]})
-    return jsonify(data_results)
+    final_date_query = session.query(func.max(func.strftime("%Y-%m-%d", Measurement.date))).all()
+    session.close()
+    max_date_string = final_date_query[0][0]
+    max_date = dt.datetime.strptime(max_date_string, "%Y-%m-%d")
+    start = max_date - dt.timedelta(365)
+    temps = final_calc(start, max_date_string)
+    end = max_date
+    final_list = []
+    date_dict = {'start_date': start, 'end_date':end}
+    final_list.append(date_dict)
+    final_list.append({'Observation': 'TMIN', 'Temperature': temps[0][0]})
+    final_list.append({'Observation': 'TAVG', 'Temperature': temps[0][1]})
+    final_list.append({'Observation': 'TMAX', 'Temperature': temps[0][2]})
+    return jsonify(final_list)
+
+if __name__ == "__main__":
+     app.run(debug=True)   
    
    
    
    
    
-   
-   
-   
+#These are codes that I tinkered on for start and start/end
     # final_date_query = session.query(func.max(func.strftime("%Y-%m-%d", Measurement.date))).all()
     # max_date_string = final_date_query[0][0]
     # max_date = dt.datetime.strptime(max_date_string, "%Y-%m-%d")
@@ -144,13 +149,7 @@ def data(start=None, end=None):
     # SEr_list.append({'Observation': 'TMIN', 'Temperature': temps[0][0]})
     # SEr_list.append({'Observation': 'TAVG', 'Temperature': temps[0][1]})
     # SEr_list.append({'Observation': 'TMAX', 'Temperature': temps[0][2]})
-    # return jsonify(SEr_list)
-    
-
-
-
-
-
+    # return jsonify(SEr_list)  
 
 # @app.route("/api/v1.0/<start>/<end>")
 # def start_end(start, end):
@@ -170,3 +169,9 @@ def data(start=None, end=None):
 #     return jsonify(return_list)
 # if __name__ == "__main__":
 #     app.run(debug=True)
+
+        # start = dt.datetime.strptime(start, "%Y-%m-%d")
+        # results = session.query(*calc_temps).\
+        #     filter(Measurement.date >= start).all()
+        # session.close()
+        # start_temps = list(np.ravel(results))
